2851226c8996cf01f991bc93a0b6833d8f47be6a
Noam Bavli <noambavli07@gmail.com>
2025-02-02 21:22:28 +0200

    Update common.h

--- ScheduleGenerator/common.h
+++ ScheduleGenerator/common.h
@@ -1,31 +1,27 @@
 #pragma once
 #include "pch.h"
 
-template<typename T> void print(const T& toPrint) {
-
-	std::cout << toPrint;
+template<typename T>
+void print(const T& toPrint) {
+    std::cout << toPrint;
 }
 
-template<typename T, typename ...Args> void print(const T& first,const  Args&... rest)
-{
-	std::cout << first ;
-	print(rest...);
-
+template<typename T, typename... Args>
+void print(const T& first, const Args&... rest) {
+    std::cout << first;
+    if constexpr (sizeof...(rest) > 0) {
+        std::cout << " ";
+        print(rest...);
+    }
 }
-
 
 char yesOrNoDialog() {
-	char answer;
-	std::cin >> answer;
-	bool keepGoing = false;
-	while (true)
-	{
-		if (answer != 'n' && answer != 'y') {
-			print("invalid answer, try again ");
-			std::cin >> answer;
-		}
-		else {
-			return answer;
-		}
-	}
+    char answer;
+    while (true) {
+        std::cin >> answer;
+        if (answer == 'y' || answer == 'n') {
+            return answer;
+        }
+        std::cout << "Invalid answer, try again: ";
+    }
 }
